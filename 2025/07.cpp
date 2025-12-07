18b950be8fd8ab8b125f863aa50babe6d2f96abd
Jx-ls <joshuarajtadi@gmail.com>
2025-12-07 19:20:14 +0530

    day 7 part 2

--- 2025/07.cpp
+++ 2025/07.cpp
@@ -6,20 +6,17 @@ void printSpace(vector<string> &space) {
 	for (auto &line : space) cout << line << endl;
 }
 
+
 int part1(vector<string> &space) {
-	int length = (int) space[0].size();
-	int lines = (int) space.size();
 	int count{0};
-	vector<int> beam(length, 0);
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < (int) space[0].size(); i++) {
 		if (space[0][i] == 'S') {
 			space[1][i] = '|';
 			break;
 		}
 	}
-	printSpace(space);
-	for (int i = 2; i < lines; i+= 2) {
-		for (int j = 0; j < length; j++) {
+	for (int i = 2; i < (int) space.size(); i+= 2) {
+		for (int j = 0; j < (int) space[0].size(); j++) {
 			if (space[i][j] == '^') {
 				if (space[i-1][j] == '|') {
 					count++;
@@ -34,7 +31,41 @@ int part1(vector<string> &space) {
 				space[i+1][j] = '|';
 			}
 		}
-		printSpace(space);
+	}
+	return count;
+}
+
+long long part2(vector<string> &space) {
+	long long count{0};
+	vector<long long> nums((int) space[0].size(), 0);
+	for (int i = 0; i < (int) space[0].size(); i++) {
+		if (space[0][i] == 'S') {
+			space[1][i] = '|';
+			nums[i] = 1;
+			break;
+		}
+	}
+	for (int i = 2; i < (int) space.size(); i+= 2) {
+		for (int j = 0; j < (int) space[0].size(); j++) {
+			if (space[i][j] == '^') {
+				if (nums[j] != 0) { 
+					space[i][j-1] = space[i-1][j];
+					nums[j-1] += nums[j];
+					space[i][j+1] = space[i-1][j];
+					nums[j+1] += nums[j];
+					space[i+1][j-1] = space[i][j-1];
+					space[i+1][j+1] = space[i][j+1];
+				}
+				nums[j] = 0;
+			}
+			else if (space[i-1][j] != '.') {
+				space[i][j] = space[i-1][j];
+				space[i+1][j] = space[i][j];
+			}
+		}
+	}
+	for (auto &num : nums) {
+		count += num;
 	}
 	return count;
 }
@@ -54,8 +85,9 @@ int main () {
 			}
 			cout << "recieved all inputs\n";
 		}
+		vector<string> space2 = space;
 		if (!input.compare("solve")) {
-			cout << part1(space) << endl;
+			cout << part1(space) << endl << part2(space2) << endl;
 		}
 	}
 }
